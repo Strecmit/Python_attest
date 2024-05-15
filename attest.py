f4871d8cfab53532157ceb1b8a6addd9152a5944
Victor <strecmit@gmail.com>
2024-05-16 01:47:59 +0200

    1

---
+++ attest.py
@@ -0,0 +1,18 @@
+import random
+import pandas as pd
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+print(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
+print(data)
+print()
+data = pd.DataFrame({'whoAmI':lst})
+data.loc[data['whoAmI'] == 'robot', 'robot_group'] = '1'
+data.loc[data['whoAmI'] != 'robot', 'robot_group'] = '0'
+data.loc[data['whoAmI'] == 'human', 'human_group'] = '1'
+data.loc[data['whoAmI'] != 'human', 'human_group'] = '0'
+
+data.head(n=20) 
+print(data)
